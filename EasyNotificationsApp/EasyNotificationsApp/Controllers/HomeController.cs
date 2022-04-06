28d440e09079400666c94a2697dcf5ab227838ec
Gaurav Dogra <gauru.dogra@gmail.com>
2022-04-06 21:24:08 +0100

    basic starter project

---
+++ EasyNotificationsApp/EasyNotificationsApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using EasyNotificationsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EasyNotificationsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
