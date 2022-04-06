8b307c65f114337773f141f3ca7677f094553337
Gaurav Dogra <gauru.dogra@gmail.com>
2022-04-06 21:41:58 +0100

    added a UserController

---
+++ EasyNotificationsApp/EasyNotificationsApp/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using EasyNotificationsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EasyNotificationsApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public string Index()
+        {
+            return "In the user controller";
+        }
+
+    }
+}
